4e90edb1f21933847a02a8fde59c16a0b486a9a8
Owenzbs <gokusay67@gmail.com>
2022-03-29 14:01:39 +0300

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     }
 }
 
-db_from_env=dj_database_url.confif()
+db_from_env=dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
